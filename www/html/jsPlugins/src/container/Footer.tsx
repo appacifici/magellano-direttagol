3c1c0d307b112dcd79e8185d6137348ed2709086
Alessandro Pacifici <ap.pacifici@gmail.com>
2024-03-26 16:59:00 +0000

    Aggiunto footer copyright

--- www/html/jsPlugins/src/container/Footer.tsx
+++ www/html/jsPlugins/src/container/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import styles 				from '../../scss/footer.module.scss'; 
 
 function Footer() {
     return( 
         <>            
-            <div>footer</div>            
+            <footer className={styles.footer}>
+                <Container className={styles.footertext}>
+                    Copyright ©️ {new Date().getFullYear()} direttagol.it di proprietà di Magellano Tech Solutions Srl - Palazzo Valadier - Piazza del Popolo, 18 - 00187 Roma RM <a href="mailto:info@magellanotech.it">info@magellanotech.it</a>
+                </Container>
+            </footer>         
         </>
     );
 }
